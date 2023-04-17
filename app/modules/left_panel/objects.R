fbf1cc8d8a82b2dbf0163851466e95639c14f978
ferroao <froao@unal.edu.co>
2023-04-17 17:34:30 -0300

    chore: add main objects

---
+++ app/modules/left_panel/objects.R
@@ -0,0 +1,24 @@
+#' @export
+phylums_per_kingdom <- readRDS("rds/phylums_per_kingdom.rds")
+
+#' @export
+classes_per_phylum <- readRDS("rds/classes_per_phylum.rds")
+
+#' @export
+orders_per_class <- readRDS("rds/orders_per_class.rds")
+
+#' @export
+families_per_order <- readRDS("rds/families_per_order.rds")
+
+#' @export
+genera_per_family <- readRDS("rds/genera_per_family.rds")
+
+#' @export
+infragenus_per_genus <- readRDS("rds/infragenus_per_genus.rds")
+
+# infragenus_ranks <-  c("species", "variety", "subspecies", "form")
+manual_ranks <- c("kingdom", "phylum", "class", "order", "family", "genus")
+# not used "unranked"
+
+#' @export
+ranks <- c(manual_ranks, "infrageneric")
